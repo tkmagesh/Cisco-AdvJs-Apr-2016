33b8b5ee903448c9378371efb55e1def79608a23
tkmagesh <tkmagesh77@gmail.com>
2016-04-11 12:02:04 +0530

    Part - 1

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,16 @@
 function add(x,y){
-	return x + y;
+	function parseArg(n){
+		if (typeof n === 'function') return parseArg(n());
+		if (n instanceof Array) {
+			var result = 0;
+			for(var i=0; i<n.length; i++)
+				result += n[i];
+			return result;
+		}
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	var result = 0;
+	for(var i=0; i<arguments.length; i++)
+		result += parseArg(arguments[i]);
+	return result;
 }
